7fc3d6595ffa5c1837fb40eaab9141cff367126a
davesheffer <dave.sheffer1@gmail.com>
2023-09-19 10:44:37 +0300

    Public All

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -5,15 +5,15 @@ import { getAuth } from 'firebase-admin/auth';
 
 const serviceAccount = {
   "type": "service_account",
-  "project_id": import.meta.env.FIREBASE_PROJECT_ID,
-  "private_key_id": import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key":import.meta.env.FIREBASE_PRIVATE_KEY,
-  "client_email": import.meta.env.FIREBASE_CLIENT_EMAIL,
-  "client_id": import.meta.env.FIREBASE_CLIENT_ID,
-  "auth_uri": import.meta.env.FIREBASE_AUTH_URI,
-  "token_uri": import.meta.env.FIREBASE_TOKEN_URI,
-  "auth_provider_x509_cert_url": import.meta.env.FIREBASE_AUTH_CERT_URL,
-  "client_x509_cert_url": import.meta.env.FIREBASE_CLIENT_CERT_URL,
+  "project_id": import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  "private_key_id": import.meta.env.FIREBASE_PUBLIC_PRIVATE_KEY_ID,
+  "private_key":import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY,
+  "client_email": import.meta.env.PUBLIC_FIREBASE_CLIENT_EMAIL,
+  "client_id": import.meta.env.PUBLIC_FIREBASE_CLIENT_ID,
+  "auth_uri": import.meta.env.PUBLIC_FIREBASE_AUTH_URI,
+  "token_uri": import.meta.env.PUBLIC_FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": import.meta.env.PUBLIC_FIREBASE_AUTH_CERT_URL,
+  "client_x509_cert_url": import.meta.env.PUBLIC_FIREBASE_CLIENT_CERT_URL,
   "universe_domain": "googleapis.com"
 };
 
