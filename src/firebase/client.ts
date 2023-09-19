39988938589b0466997b81713efc910492734049
davesheffer <dave.sheffer1@gmail.com>
2023-09-19 10:07:02 +0300

    commit API KEY

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "hound-hub.firebaseapp.com",
   projectId: "hound-hub",
   storageBucket: "hound-hub.appspot.com",
   messagingSenderId: "846272140953",
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
+  appId: import.meta.env.FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig, 'hound-hub');
